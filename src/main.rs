d32adbed04996793251c6c600157c3c2178d07fb
Velvid <muntakim.ca@gmail.com>
2023-08-15 23:39:43 -0400

    extendable character pools

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 mod pwdgen;
-use crate::pwdgen::gen_pwd;
 
 use clap::Parser;
 
@@ -44,39 +43,56 @@ struct PwdArgs {
 fn main() {
     let args = PwdArgs::parse();
 
+    let pools = [
+        pwdgen::Pool {
+            name: "alpha",
+            minimum: args.min_alpha,
+            chars: pwdgen::ALPHA,
+        },
+        pwdgen::Pool {
+            name: "numeric",
+            minimum: args.min_numeric,
+            chars: pwdgen::NUMERIC,
+        },
+        pwdgen::Pool {
+            name: "special",
+            minimum: args.min_special,
+            chars: pwdgen::SPECIAL,
+        },
+    ];
+
     let start = std::time::Instant::now();
 
-    let pwd = gen_pwd(
-        &mut rand::thread_rng(),
-        args.length,
-        args.min_alpha,
-        args.min_numeric,
-        args.min_special,
-    );
+    let pwd = pwdgen::gen_pwd_from_pools(&mut rand::thread_rng(), args.length, &pools);
 
     let elapsed = start.elapsed();
 
-    println!("{}", pwd,);
+    println!("{}", pwd);
 
     if args.verbose {
-        // Print elapsed time to generate password.
-        print!("Took: ");
-        match elapsed.as_nanos() {
-            0..=999 => println!("{} ns", elapsed.as_nanos()),
-            1_000..=999_999 => println!("{} us", elapsed.as_micros()),
-            1_000_000..=999_999_999 => println!("{} ms", elapsed.as_millis()),
-            _ => println!("{} s", elapsed.as_secs()),
+        // Elapsed time to generate password.
+        let mut time = elapsed.as_nanos() as f64;
+        let mut units = vec!["s", "ms", "μs", "ns"];
+
+        while !(time < 1000.0) && units.len() > 1 {
+            time /= 1000.0;
+            units.pop();
         }
 
-        // Print percentage of each character type.
-        let alpha_count = pwd.chars().filter(|c| pwdgen::ALPHA.contains(*c)).count();
-        let numeric_count = pwd.chars().filter(|c| pwdgen::NUMERIC.contains(*c)).count();
-        let special_count = pwd.chars().filter(|c| pwdgen::SPECIAL.contains(*c)).count();
-        println!(
-            "Alpha: {:.2}%, Numeric: {:.2}%, Special: {:.2}%",
-            alpha_count as f64 / pwd.len() as f64 * 100.0,
-            numeric_count as f64 / pwd.len() as f64 * 100.0,
-            special_count as f64 / pwd.len() as f64 * 100.0,
-        );
+        println!("Time to generate: {:.1} {}", time, units.last().unwrap());
+
+        // Percentage of each character pool in final password.
+        let mut percentages: Vec<String> = Vec::with_capacity(pools.len());
+
+        let pool_counts = pools
+            .iter()
+            .map(|p| pwd.chars().filter(|c| p.chars.contains(*c)).count());
+
+        for (pool, count) in pools.iter().zip(pool_counts) {
+            let percent = (count as f64 / pwd.len() as f64) * 100.0;
+            percentages.push(format!("{}: {:.1}%", pool.name, percent));
+        }
+
+        println!("Distribution: {{ {} }}", percentages.join(", "));
     }
 }
--- src/pwdgen.rs
+++ src/pwdgen.rs
@@ -1,117 +1,84 @@
-use rand::{seq::IteratorRandom, seq::SliceRandom};
-use std::cmp;
-use weighted_rand::builder::*;
+use constcat::concat;
+use rand::seq::{IteratorRandom, SliceRandom};
 
-pub const ALPHA: &'static str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// Sample character pools.
+pub const UPPER: &'static str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+pub const LOWER: &'static str = "abcdefghijklmnopqrstuvwxyz";
+pub const ALPHA: &'static str = concat!(UPPER, LOWER);
 pub const NUMERIC: &'static str = "0123456789";
 pub const SPECIAL: &'static str = "!@#$%^&*()_+-=[]{};':\",./<>?\\|";
 
-pub fn gen_pwd(
-    rng: &mut rand::rngs::ThreadRng,
-    min_length: usize,
-    min_alpha: usize,
-    min_numeric: usize,
-    min_special: usize,
-) -> String {
+pub struct Pool {
+    pub name: &'static str,
+    pub minimum: usize,
+    pub chars: &'static str,
+}
+
+pub fn gen_pwd_from_pools<R>(rng: &mut R, min_length: usize, pools: &[Pool]) -> String
+where
+    R: rand::Rng + ?Sized,
+{
     // Get the maximum length of the password.
-    let min_sum = min_alpha + min_numeric + min_special;
-    let length = cmp::max(min_length, min_sum);
+    let min_sum = pools.iter().map(|p| p.minimum).sum::<usize>();
+    let length = std::cmp::max(min_length, min_sum);
 
-    // Populate password from character sets.
-    let mut pwd: Vec<char> = Vec::new();
-    pwd.reserve(length);
+    // Reserve a vector for the password.
+    let mut pwd: Vec<char> = Vec::with_capacity(length);
 
-    let sets = [ALPHA, NUMERIC, SPECIAL];
-    let mins = [min_alpha, min_numeric, min_special];
-    let weights = sets.map(|s| s.len() as u32);
-    let table = WalkerTableBuilder::new(&weights).build();
-
-    for (set, min) in sets.iter().zip(mins.iter()) {
-        for _ in 0..*min {
-            let c = set.chars().choose(rng).unwrap();
+    // Add the minimum number of characters from each pool.
+    for pool in pools {
+        for _ in 0..pool.minimum {
+            let c = pool.chars.chars().choose(rng).unwrap();
             pwd.push(c);
         }
     }
 
-    let remainder = length - min_sum;
-    for _ in 0..remainder {
-        let i = table.next_rng(rng);
-        let c = sets[i].chars().choose(rng).unwrap();
+    // Add the remaining characters.
+    let merged_chars: String = pools.iter().flat_map(|p| p.chars.chars()).collect();
+
+    for _ in 0..(length - min_sum) {
+        let c = merged_chars.chars().choose(rng).unwrap();
         pwd.push(c);
     }
 
+    // Since characters were added in order, shuffle the password.
     pwd.shuffle(rng);
 
-    // Return password as a string.
-    pwd.into_iter().collect::<String>()
+    // Return as a string.
+    pwd.into_iter().collect()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    fn test_rng() -> rand::rngs::ThreadRng {
-        // Can change this to a different RNG if needed.
-        rand::thread_rng()
-    }
-
     #[test]
-    fn normal_case() {
-        let pwd = gen_pwd(&mut test_rng(), 32, 8, 8, 8);
-        let alpha_count = pwd.chars().filter(|c| ALPHA.contains(*c)).count();
-        let numeric_count = pwd.chars().filter(|c| NUMERIC.contains(*c)).count();
-        let special_count = pwd.chars().filter(|c| SPECIAL.contains(*c)).count();
+    fn general() {
+        let mut rng = rand::thread_rng();
 
-        assert!(alpha_count >= 8);
-        assert!(numeric_count >= 8);
-        assert!(special_count >= 8);
-        assert_eq!(pwd.len(), 32);
-    }
+        let pools = [
+            Pool {
+                name: "alpha",
+                minimum: 2,
+                chars: ALPHA,
+            },
+            Pool {
+                name: "numeric",
+                minimum: 2,
+                chars: NUMERIC,
+            },
+            Pool {
+                name: "special",
+                minimum: 2,
+                chars: SPECIAL,
+            },
+        ];
 
-    #[test]
-    fn larger_min_total() {
-        let pwd = gen_pwd(&mut test_rng(), 16, 8, 8, 8);
-        let alpha_count = pwd.chars().filter(|c| ALPHA.contains(*c)).count();
-        let numeric_count = pwd.chars().filter(|c| NUMERIC.contains(*c)).count();
-        let special_count = pwd.chars().filter(|c| SPECIAL.contains(*c)).count();
+        let pwd = gen_pwd_from_pools(&mut rng, 8, &pools);
 
-        assert_eq!(alpha_count, 8);
-        assert_eq!(numeric_count, 8);
-        assert_eq!(special_count, 8);
-        assert_eq!(pwd.len(), 24);
-    }
-
-    #[test]
-    fn all_alpha() {
-        let pwd = gen_pwd(&mut test_rng(), 16, 16, 0, 0);
-        let count = pwd.chars().filter(|c| ALPHA.contains(*c)).count();
-
-        assert!(count == 16);
-        assert!(pwd.len() == 16);
-    }
-
-    #[test]
-    fn all_numeric() {
-        let pwd = gen_pwd(&mut test_rng(), 16, 0, 16, 0);
-        let count = pwd.chars().filter(|c| NUMERIC.contains(*c)).count();
-
-        assert!(count == 16);
-        assert!(pwd.len() == 16);
-    }
-
-    #[test]
-    fn all_special() {
-        let pwd = gen_pwd(&mut test_rng(), 16, 0, 0, 16);
-        let count = pwd.chars().filter(|c| SPECIAL.contains(*c)).count();
-
-        assert!(count == 16);
-        assert!(pwd.len() == 16);
-    }
-
-    #[test]
-    fn zero_min() {
-        let pwd = gen_pwd(&mut test_rng(), 16, 0, 0, 0);
-
-        assert!(pwd.len() == 16);
+        assert_eq!(pwd.len(), 8);
+        for pool in &pools {
+            assert!(pwd.chars().filter(|c| pool.chars.contains(*c)).count() >= pool.minimum);
+        }
     }
 }
